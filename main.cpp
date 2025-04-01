9d931430069808ad22957adb99ab8f72ab93e811
ersek <ersek@simplesoft.ss>
2025-04-01 13:19:23 +0200

    change "unsigned char" to uint8_t

--- main.cpp
+++ main.cpp
@@ -4,26 +4,26 @@
 #include "./lib/compress.h"
 
 int main() {
-  const std::vector<unsigned char> data = {1, 2, 2, 2, 3, 4, 4, 5};
+  const std::vector<uint8_t> data = {1, 2, 2, 2, 3, 4, 4, 5};
 
   // Test RLE
   compress::Compressor *rleCompressor = compress::createRLECompressor();
-  std::vector<unsigned char> rleCompressed = rleCompressor->compress(data);
-  std::vector<unsigned char> rleDecompressed = rleCompressor->decompress(rleCompressed);
+  const std::vector<uint8_t> rleCompressed = rleCompressor->compress(data);
+  std::vector<uint8_t> rleDecompressed = rleCompressor->decompress(rleCompressed);
   std::cout << "RLE: Original size: " << data.size() << ", Compressed size: " << rleCompressed.size() << std::endl;
   delete rleCompressor;
 
   // Test LZW
   compress::Compressor *lzwCompressor = compress::createLZWCompressor();
-  std::vector<unsigned char> lzwCompressed = lzwCompressor->compress(data);
-  std::vector<unsigned char> lzwDecompressed = lzwCompressor->decompress(lzwCompressed);
+  const std::vector<uint8_t> lzwCompressed = lzwCompressor->compress(data);
+  std::vector<uint8_t> lzwDecompressed = lzwCompressor->decompress(lzwCompressed);
   std::cout << "LZW: Original size: " << data.size() << ", Compressed size: " << lzwCompressed.size() << std::endl;
   delete lzwCompressor;
 
   // Test Huffman
   compress::Compressor *huffmanCompressor = compress::createHuffmanCompressor();
-  std::vector<unsigned char> huffmanCompressed = huffmanCompressor->compress(data);
-  std::vector<unsigned char> huffmanDecompressed = huffmanCompressor->decompress(huffmanCompressed);
+  const std::vector<uint8_t> huffmanCompressed = huffmanCompressor->compress(data);
+  std::vector<uint8_t> huffmanDecompressed = huffmanCompressor->decompress(huffmanCompressed);
   std::cout << "Huffman: Original size: " << data.size() << ", Compressed size: " << huffmanCompressed.size()
             << std::endl;
   delete huffmanCompressor;
